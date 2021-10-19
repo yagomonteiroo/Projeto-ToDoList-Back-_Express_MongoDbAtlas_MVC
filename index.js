894daf0d9e2186af0032690579017f32418355f4
yago.monteiroo@gmail.com <yago.monteiroo@gmail.com>
2021-10-18 22:40:57 -0300

    configuração das variáveis de ambiente

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+if(process.env.NODE_ENV !== 'production'){
+require('dotenv').config();
+}
 const express = require("express");
 const cors = require("cors");
 const Conn = require ("./conn/Conn");
@@ -6,12 +9,18 @@ const TodoRoutes =  require("./routes/Todo.routes");
 const app = express();
 app.use(express.json());
 app.use(cors());
-Conn();
+
+const db_url= process.env.DB_URL;
+const db_user= process.env.DB_USER;
+const db_pass= process.env.DB_PASS;
+const db_data= process.env.DB_DATA;
+
+Conn(db_url, db_user, db_pass, db_data);
 
 app.use("/tarefas",TodoRoutes);
 
-const port = 3000;
+const port = 3001;
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
    console.info(`O app está rodando na porta http://localhost:${port}/`);
 });
